c1017e672c383dd5cefb012124d13861e54584a6
BabiryePolyn <babiryepolyn@gmail.com>
2025-08-08 11:11:26 +0300

    Task 2: Assignment 1

---
+++ No2.py
@@ -0,0 +1,11 @@
+numbers = [5, 15, 25, 35, 45]
+print("Original list:")
+print(numbers)
+
+numbers.insert(2, 20)
+print("\nAfter inserting 20 at correct position:")
+print(numbers)
+
+numbers.pop()
+print("\nAfter removing the last item:")
+print(numbers)
