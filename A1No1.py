065cfbf4b3726ddb885d676926dd5406c10e7bb8
BabiryePolyn <babiryepolyn@gmail.com>
2025-08-08 11:15:14 +0300

    Task 2 Assignment 1

---
+++ A1No1.py
@@ -0,0 +1,11 @@
+movies = ["The Matrix", "Inception", "Avatar", "Titanic", "Interstellar"]
+print("Original movie list:")
+print(movies)
+
+movies[2] = "The Dark Knight"
+print("\nAfter replacing movie at index 2:")
+print(movies)
+
+movies.sort()
+print("\nAfter sorting alphabetically:")
+print(movies)
